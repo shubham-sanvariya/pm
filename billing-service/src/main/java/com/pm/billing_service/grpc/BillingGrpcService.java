178cdae1da281666c38c61c58153e70935708d7e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-23 20:15:11 +0530

    Refactor BillingGrpcService to remove unused imports and methods

--- billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
@@ -1,32 +1,4 @@
 package com.pm.billing_service.grpc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import billing.BillingRequest;
-import billing.BillingResponse;
-import billing.BillingServiceGrpc.BillingServiceImplBase;
-import io.grpc.stub.StreamObserver;
-import net.devh.boot.grpc.server.service.GrpcService;
-
-@GrpcService
-public class BillingGrpcService extends BillingServiceImplBase{
-
-    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
-
-    @Override
-    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
-        log.info("createBillingAccount request received {}", request.toString());
-
-        // Business logic e.g. save to database
-
-        BillingResponse response = BillingResponse.newBuilder()
-            .setAccountId("12345")
-            .setStatus("ACTIVE")
-            .build();
-
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
-    
+public class BillingGrpcService {
 }
