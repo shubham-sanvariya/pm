f415a595ab34ca3860f5e1a407163f04353902de
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-26 20:47:40 +0530

    Add AuthController for user login handling with token generation response

---
+++ auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.pm.auth_service.controller;
+
+import com.pm.auth_service.dto.LoginRequestDTO;
+import com.pm.auth_service.dto.LoginResponseDTO;
+import com.pm.auth_service.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO dto){
+        Optional<String> tokenOptional = authService.authenticate(dto);
+
+        if (tokenOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
