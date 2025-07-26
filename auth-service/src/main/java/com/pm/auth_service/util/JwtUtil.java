6bbbe553fcce50be75980d312f9c03e3378cbc79
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-26 20:45:36 +0530

    Annotate JwtUtil class with @Component for Spring dependency injection

--- auth-service/src/main/java/com/pm/auth_service/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/auth_service/util/JwtUtil.java
@@ -3,12 +3,14 @@ package com.pm.auth_service.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
     private final Key secretKey;
